96a1759ab07debd21eaf17d3397cbbc4efdf2762
Tanner <troxs690@gmail.com>
2022-01-19 15:53:56 -0600

    done

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
- const cats = ["Milo", "Otis", "Garfield"] 
- 
-function destructivelyAppendCat(name) {
-    cats.push("Ralph"); }
-
-    function destructivelyPrependCat(name) {
-    cats.unshift("Bob") }
-
- function  destructivelyRemoveLastCat(name) {
-     cats.pop("Ralph")
- }
-function destructivelyRemoveFirstCat(name) {
-    cats.shift("Milo")
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+const allCats = [...cats, "Broom"]
+function appendCat() {
+    return allCats
 }
 
-function appendCat(name){
-    const copyOfCats = [...cats, 'Broom'];
-    return copyOfCats; }
-
-    function prependCat(name){
-        const copyOfCats = ['Arnold', ...cats];
-        return copyOfCats; }
-        
-        
-function removeLastCat(name){
-    const copyOfCats = cats.slice(0,-1);
-    return copyOfCats; }
-
-    function removeFirstCat(){
-        return cats.slice(1) }
-    
+const mycats = ['Arnold', ...cats]
+function prependCat() {
+    return mycats
+}
+function removeLastCat(name) {
+    const mycats = cats.slice(0,-1)
+return mycats
+}
+function removeFirstCat(){
+    return cats.slice(1) }
